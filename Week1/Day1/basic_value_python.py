0ecc8a99f821b0588e58103c3e213e980b97a49c
Sofia <sofisinach@gmail.com>
2025-05-11 10:29:15 +0300

    exercise string

---
+++ Week1/Day1/basic_value_python.py
@@ -0,0 +1,18 @@
+#"hello python".capitalize()
+#print ("hello python".capitalize())
+
+#x = "I like apples"
+#txt = x.replace("apples", "bananas")
+#print (txt) 
+
+#exercise string
+description = "strings are..."
+upp = description.upper()
+place = description.replace ("are", "is")
+place1 = description[0:7] #slicing
+print (upp, place, place1)
+
+
+
+
+
